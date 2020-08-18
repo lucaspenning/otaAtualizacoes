8aa848c78c322bd548bf599a20ff531e63e9d2a8
lucaspenning <50622975+lucaspenning@users.noreply.github.com>
2020-08-18 13:09:48 -0300

    Update exemplo.py

--- src/exemplo.py
+++ src/exemplo.py
@@ -1,7 +1,32 @@
 
-import utime
+## Nome: Pisca_Thread.py
+## Autor: Victor Hugo Lima
+## Data: 13/05/2018
+## Finalidade: Est茅 programa faz piscar dois leds em tempos diferentes atrav茅s de threads.
 
-for valor in range(10):
-    print(valor)
-    utime.sleep(2)
+import _thread
+import utime
+import machine
+pin21 = machine.Pin(21, machine.Pin.OUT)
+pin19 = machine.Pin(19, machine.Pin.OUT)
+ 
+def pisca1():
+	while True:
+		pin21.value(1)
+		utime.sleep_ms(500)
+		pin21.value(0)
+		utime.sleep_ms(500)
+def pisca2():
+	while True:
+
+		pin19.value(1)
+		utime.sleep_ms(50)
+
+		pin19.value(0)
+
+		utime.sleep_ms(50)
+
+_thread.start_new_thread(pisca1, ())
+
+_thread.start_new_thread(pisca2, ())
 
