96fa8ba3d29bfab530a3836852d2f9541933c9fd
lucaspenning <50622975+lucaspenning@users.noreply.github.com>
2020-08-18 13:28:56 -0300

    Update exemplo.py

--- src/exemplo.py
+++ src/exemplo.py
@@ -13,18 +13,18 @@ pin19 = machine.Pin(19, machine.Pin.OUT)
 def pisca1():
 	while True:
 		pin21.value(1)
-		utime.sleep_ms(500)
+		utime.sleep_ms(250)
 		pin21.value(0)
-		utime.sleep_ms(500)
+		utime.sleep_ms(250)
 def pisca2():
 	while True:
 
 		pin19.value(1)
-		utime.sleep_ms(50)
+		utime.sleep_ms(150)
 
 		pin19.value(0)
 
-		utime.sleep_ms(50)
+		utime.sleep_ms(150)
 
 _thread.start_new_thread(pisca1, ())
 
