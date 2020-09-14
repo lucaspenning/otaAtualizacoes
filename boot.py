160b1018ae5365d930913ee0ddfaf0b1fe32ef41
lucaspenning <50622975+lucaspenning@users.noreply.github.com>
2020-09-14 14:23:49 -0300

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,6 +1,3 @@
-
-
-
 #
 
 import os
@@ -9,6 +6,7 @@ import machine
 from umqtt.simple import MQTTClient
 from code_download import *
 
+#Verifica se existe o arquivo de versionamento
 try:
     f = open('src/.version')
     f.close()
@@ -16,9 +14,12 @@ try:
 except:
     verifica = 0
 
+#Configurar o Broker MQTT
 mq = MQTTClient("AtualizacaoOTA","ioticos.org",1883,"G63VllTzA4939FY","46pnhP9OlrHX7vZ")
 
+#Led VERDE(ATUALIZACOES)
 pin17 = machine.Pin(17, machine.Pin.OUT)
+#Led AMARELO(STATUS DA ATUALIZACAO)
 pin18 = machine.Pin(18, machine.Pin.OUT)
 pin18.value(1)
 
@@ -27,7 +28,8 @@ if(verifica == 1):
   version = f.read()
   print(version)
   f.close()
-
+  
+#Baixando e Atualizando os codigos
 try:
     c = Code_download()
     c.download_update()
@@ -44,14 +46,16 @@ if(verifica == 1):
   version_v = g.read()
   print(version_v)
   g.close()
-
-
+  
+#Virificando atualizacoes
 if(verifica == 1):
   if(version != version_v):
     print("Novas atualizacoes")
+    #Conectando ao Broker MQTT e publicando 
     mq.connect()
     mq.publish(b"oazafNBJA98GhyJ",b"Sistema reiniciado, NOVO atualizado, versao "+str(version_v))
     utime.sleep(1)
+    #Rotina de piscagem do LED VERDE
     pin17.value(1)
     utime.sleep_ms(1000)
     pin17.value(0)
@@ -61,9 +65,11 @@ if(verifica == 1):
     pin17.value(0)
   elif(version == version_v):
     print("Sem novas atualizacoes")
+    #Conectando ao Broker MQTT e publicando
     mq.connect()
     mq.publish(b"oazafNBJA98GhyJ",b"Sistema reiniciado, SEM atualizado, versao "+str(version))
     utime.sleep(1)
+    #Rotina de piscagem do LED VERDE
     pin17.value(1)
     utime.sleep_ms(1000)
     pin17.value(0)
